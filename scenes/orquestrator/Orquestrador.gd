276025a4dc8b7582f0f311c37b78cca59ee95f9b
Raí Weslley <79660844+ggxrc@users.noreply.github.com>
2025-05-11 18:52:38 -0300

    Mudanças no código do orquestrador

--- scenes/orquestrator/Orquestrador.gd
+++ scenes/orquestrator/Orquestrador.gd
@@ -1,12 +1,18 @@
 # /cenas/Orquestrador.gd
 extends Node
-
+	
 @onready var menu_principal = $MenuPrincipal
 @onready var level_selection = $LevelSelection
+@onready var prologue = $Prologue
 
 func _on_voltar_menu_pressed() -> void:
 	menu_principal.visible = true
 
-
 func _on_iniciar_pressed() -> void:
+	prologue.visible = true
 	menu_principal.visible = false
+	level_selection.visible = false
+	
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
