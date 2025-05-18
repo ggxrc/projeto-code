38ce5808e84de9dba7b18c1668b941c98623e580
Raí Weslley <79660844+ggxrc@users.noreply.github.com>
2025-05-18 16:56:31 -0300

    algumas mudanças
    não sei como as cameras funcionam (ainda)

--- scenes/orquestrator/Orquestrador.gd
+++ scenes/orquestrator/Orquestrador.gd
@@ -4,10 +4,9 @@ extends Node
 @onready var menu_principal = $MenuPrincipal
 @onready var level_selection = $LevelSelection
 @onready var prologue = $Prologue
-
+@onready var camera = Camera2D
 var scenes: Array
 var next_scene: Node
-var current_scene: Node
 
 func _ready() -> void:
 	scenes = [
@@ -18,13 +17,11 @@ func _ready() -> void:
 
 func _hide_all_scenes() -> void:
 	for scene in scenes:
-		next_scene = scene
-		next_scene.queue_free()
-		next_scene = null
+		scene.visible = false
 	
 func scene_transition(next_scene: Node) -> void:
 	_hide_all_scenes()
-	add_child(next_scene)
+	next_scene.visible = true
 	self.next_scene = next_scene 
 
 func _on_voltar_menu_pressed() -> void:
