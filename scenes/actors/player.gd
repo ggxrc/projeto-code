50e9aeca9eb1e0f3b7cfdbb53bfbcb32f4ed7b95
ggxrc <79660844+ggxrc@users.noreply.github.com>
2025-05-27 13:15:02 -0300

    mudanças no prólogo

--- scenes/actors/player.gd
+++ scenes/actors/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
-	_move()
+	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	if dir:
+		velocity = dir * 200
+	else:
+		velocity.x = move_toward(velocity.x, 0,200)
+		velocity.y = move_toward(velocity.y, 0,200)
 	
-func _move() -> void:
-	var _direction: Vector2 = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
+	move_and_slide()
+	
